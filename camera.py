07ce3a623553db1c76f9aff50934169afdbf3dff
Zetryx97 <gablessard17@gmail.com>
2022-10-20 12:23:02 -0400

    Co-authored-by: GuillaumeLegare <GuillaumeLegare@users.noreply.github.com> Co-authored-by: SamyTetrault <SamyTetrault@users.noreply.github.com>

--- camera.py
+++ camera.py
@@ -13,6 +13,8 @@ class Caméra:
         self.modele = cv2.cvtColor(cv2.imread("modele-objet.bmp"),cv2.COLOR_BGR2GRAY)
         self.lit_une_image = True
         self.image_modele = None
+        self.mask = cv2.imread("mask-objet.bmp")
+        self.frame_roi = None
         
     def set_resolution_camera(self):
         self.vcap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
@@ -37,23 +39,21 @@ class Caméra:
 
     def trouver_modele(self):
         self.set_resolution_camera()
-    
         while self.lit_une_image:
             ok,image = self.vcap.read()
             if not ok:
                 print("Erreur avec l'image!")
                 break
-            cv2.imshow("Labo 5", image)
-            image_en_gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            #print(f"img:{image_en_gris.shape}")   
-            #print(f"mod:{self.modele.shape}")   
+            image_en_gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
             res = cv2.matchTemplate(image_en_gris, self.modele, cv2.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            self.frame_roi = [min_loc[1]:max_loc[1], min_loc[0]:max_loc[0]]
             touche = cv2.waitKey(33)
+            image_roi = cv2.rectangle(image,self.frame_roi[0],self.frame_roi[1],(175,175,175),2) 
+            cv2.imshow("Labo 5", image_roi)
             if touche == ord('q'):
                 self.lit_une_image = False
                 break
-        print(max_loc)
         self.vcap.release()
         cv2.destroyAllWindows()
 
